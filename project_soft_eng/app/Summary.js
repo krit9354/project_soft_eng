c4cad2e030ce55226ee9d6aa789c70aa6b22e820
Sahirann <s6501012630182@email.kmutnb.ac.th>
2024-09-26 05:32:57 +0700

    update Summary with no graph

--- project_soft_eng/app/Summary.js
+++ project_soft_eng/app/Summary.js
@@ -1,36 +1,128 @@
-import { View, Text, Image, TextInput, } from 'react-native';
+import { View, Text, Image, TextInput, ScrollView, TouchableOpacity } from 'react-native';
+import { Picker } from "@react-native-picker/picker";
 import { useState } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import { myStyle } from '../style/summary_style';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import dayjs from 'dayjs';
+
 
 
 export default function Summary() {
   const moneyIn = 2000
   const moneyOut = 1000
+  const [selectedGroup, setSelectedGroup] = useState("");
+
+  const [dateStart, setDateStart] = useState(new Date());
+  const [isDateStartPickerVisible, setDateStartPickerVisibility] = useState(false);
+  const showDateStartPicker = () => { setDateStartPickerVisibility(true); };
+  const hideDateStartPicker = () => { setDateStartPickerVisibility(false); };
+  function handleConfirmDateStart(date) {
+    setDateStart(date);
+    hideDateStartPicker();
+  }
+
+  const [dateEnd, setDateEnd] = useState(new Date());
+  const [isDateEndPickerVisible, setDateEndPickerVisibility] = useState(false);
+  const showDateEndPicker = () => { setDateEndPickerVisibility(true); };
+  const hideDateEndPicker = () => { setDateEndPickerVisibility(false); };
+  function handleConfirmDateEnd(date) {
+    setDateEnd(date);
+    hideDateEndPicker();
+  }
+
+
   return (
+
     <LinearGradient
-      colors={['#CDFADB', '#38E298']}
+      colors={["#CDFADB", "#38E298"]}
       locations={[0.75, 1]}
       start={{ x: 0.5, y: 0 }}
       end={{ x: 0.5, y: 1 }}
-      style={myStyle.bg}>
-      {/* top bar */}
-      <View style={myStyle.bg_white}>
-        <Text style={{ fontSize: 30, fontWeight: 'bold', margin: 20 }}>Summary</Text>
-        <TextInput placeholder="Name"></TextInput>
-        <View style={myStyle.containerShow}>
-          <View style={myStyle.box}>
-            <Text>รวมเงินเข้า (บาท)</Text>
-            <Text>{moneyIn}</Text>
-          </View>
-          <View style={myStyle.box}>
-            <Text>รวมเป็นออก (บาท)</Text>
-            <Text>{moneyOut}</Text>
+      style={myStyle.bg}
+    >
+      {/* main content */}
+      <ScrollView showsVerticalScrollIndicator={false} style={myStyle.main_content_box} >
+        <View style={myStyle.main_pocket}>
+          <Text style={{ fontSize: 30, fontWeight: 'bold', margin: 20 }}>Summary</Text>
+          <View style={myStyle.containerInput}>
+            <View style={myStyle.pickerContainer}>
+              <Picker
+                selectedValue={selectedGroup}
+                onValueChange={(itemValue) => setSelectedGroup(itemValue)}
+                style={myStyle.picker}
+              >
+                <Picker.Item label="รายรับ-รายจ่าย" value="in_out" />
+                <Picker.Item label="รายรับ" value="in" />
+                <Picker.Item label="รายจ่าย" value="out" />
+              </Picker>
+            </View>
+            <View style={myStyle.containerDate}>
+
+              <TouchableOpacity onPress={showDateStartPicker} style={myStyle.inputDate}>
+                <Text style={myStyle.textDate}>{dateStart.toDateString()}</Text>
+              </TouchableOpacity>
+              <DateTimePickerModal
+                isVisible={isDateStartPickerVisible}
+                mode="date"
+                date={dateStart}
+                onConfirm={handleConfirmDateStart}
+                onCancel={hideDateStartPicker}
+              />
+              <Text style={myStyle.textDash}>-</Text>
+              <TouchableOpacity onPress={showDateEndPicker} style={myStyle.inputDate}>
+                <Text style={myStyle.textDate}>{dateEnd.toDateString()}</Text>
+              </TouchableOpacity>
+              {/* <Text onPress={showDateEndPicker} style={myStyle.inputDate}>{dateEnd.toDateString()}</Text> */}
+              <DateTimePickerModal
+                isVisible={isDateEndPickerVisible}
+                mode="date"
+                date={dateEnd}
+                onConfirm={handleConfirmDateEnd}
+                onCancel={hideDateEndPicker}
+              />
+            </View>
+            <View style={myStyle.containerButtonFilter}>
+              <TouchableOpacity style={myStyle.button}>
+                <Text style={myStyle.buttonText}>Search</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={myStyle.button}>
+                <Text style={myStyle.buttonText}>Clear</Text>
+              </TouchableOpacity>
+
+            </View>
+            <View style={myStyle.containerShow}>
+              <View style={myStyle.box}>
+                <Text style={{ fontSize: 15 }}>รวมเงินเข้า (บาท)</Text>
+                <Text style={{ fontSize: 15 }}>{moneyIn}</Text>
+                <View style={{ justifyContent: "space-between", flexDirection: 'row', marginTop: '2%' }}>
+                  <Text style={{ fontSize: 12 }}>รายการ</Text>
+                  <Text style={{ fontSize: 12 }}>113</Text>
+                </View>
+                <View style={{ justifyContent: "space-between", flexDirection: 'row', }}>
+                  <Text style={{ fontSize: 12 }}>เฉลี่ย/เดิอน</Text>
+                  <Text style={{ fontSize: 12 }}>113</Text>
+                </View>
+              </View>
+              <View style={myStyle.box}>
+                <Text style={{ fontSize: 15 }}>รวมเป็นออก (บาท)</Text>
+                <Text style={{ fontSize: 15 }}>{moneyOut}</Text>
+                <View style={{ justifyContent: "space-between", flexDirection: 'row', marginTop: '2%' }}>
+                  <Text style={{ fontSize: 12 }}>รายการ</Text>
+                  <Text style={{ fontSize: 12 }}>113</Text>
+                </View>
+                <View style={{ justifyContent: "space-between", flexDirection: 'row', }}>
+                  <Text style={{ fontSize: 12 }}>เฉลี่ย/เดิอน</Text>
+                  <Text style={{ fontSize: 12 }}>113</Text>
+                </View>
+              </View>
+            </View>
           </View>
         </View>
+      </ScrollView>
 
+      {/* bottom bar */}
 
-      </View>
     </LinearGradient>
   );
 }
