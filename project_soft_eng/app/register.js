784089e1754bf30d9cfc329a30e7bd0ae0a5090e
Sahirann <s6501012630182@email.kmutnb.ac.th>
2024-11-04 02:23:54 +0700

    check input register is email

--- project_soft_eng/app/register.js
+++ project_soft_eng/app/register.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, Image, TouchableOpacity,TextInput} from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity, TextInput } from 'react-native';
 import { myStyle } from '../style/register_style';
 import { LinearGradient } from 'expo-linear-gradient';
 import PocketCard from '../components/pocketCard';
@@ -12,154 +12,95 @@ import { router } from 'expo-router';
 import validator from 'validator';
 
 export default function Register() {
-    const { signUp } = useSession();
+  const { signUp } = useSession();
 
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirm_password, setConfirm_password] = useState('');
-    function validateEmail(email) {
-      var re = /\S+@\S+\.\S+/;
-      console.log(validator.isEmail(email))
-      console.log(re.test(email))
-      return re.test(email);
-    }
-    async function  register  (){
-      try{
-        console.log(!validator.isEmail(email))
-        // if (!validator.isEmail(email)) {
-        //   console.log("Invalid email")
-        //   return Alert.alert("Invalid email");
-        // }
-        if (password != confirm_password) {
-          return Alert.alert("Passwords don't match");
-        }
-        console.log("REGISTERING!!!")
-        await signUp(email, username, password, confirm_password);
-      } catch (err) {
-        // console.log("err_register :",err.message)
-        Alert.alert("Register not successful");
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirm_password, setConfirm_password] = useState('');
+
+
+  async function register() {
+    try {
+      console.log(!validator.isEmail(email))
+      if (!validator.isEmail(email)) {
+        console.log("Invalid email")
+        return alert("Invalid email");
       }
-    };
-    // validate = (text) => {
-    //   console.log(text);
-    //   let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
-    //   if (reg.test(text) === false) {
-    //     console.log("Email is Not Correct");
-    //     this.setState({ email: text })
-    //     return false;
-    //   }
-    //   else {
-    //     this.setState({ email: text })
-    //     console.log("Email is Correct");
-    //   }
-    // }
-    return (
+      if (password != confirm_password) {
+        return alert("Passwords don't match");
+      }
+      console.log("REGISTERING!!!")
+      await signUp(email, username, password, confirm_password);
+    } catch (err) {
+      // console.log("err_register :",err.message)
+      alert("Register not successful");
+    }
+  };
+  // validate = (text) => {
+  //   console.log(text);
+  //   let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w\w+)+$/;
+  //   if (reg.test(text) === false) {
+  //     console.log("Email is Not Correct");
+  //     this.setState({ email: text })
+  //     return false;
+  //   }
+  //   else {
+  //     this.setState({ email: text })
+  //     console.log("Email is Correct");
+  //   }
+  // }
+  return (
 
-        <LinearGradient
-        colors={['#CDFADB', '#38E298']}
-        locations={[0.75, 1]}
-        start={{ x: 0.5, y: 0 }}
-        end={{ x: 0.5, y: 1 }}
-        style={myStyle.bg}>
-            {/* top bar */}
+    <LinearGradient
+      colors={['#CDFADB', '#38E298']}
+      locations={[0.75, 1]}
+      start={{ x: 0.5, y: 0 }}
+      end={{ x: 0.5, y: 1 }}
+      style={myStyle.bg}>
+      {/* top bar */}
 
-            <View style={myStyle.bg}>
+      <View style={myStyle.bg}>
         <View style={myStyle.main_pocket}>
-          
-          <Text style={{fontSize:32}}>I have money</Text>
-           <TextInput
-        value={email}
-        placeholder="email"
-        onChangeText={text => setEmail(text)}
-        style={myStyle.pocket}
-           />
-             <TextInput
-        value={username}
-        placeholder="Username"
-        onChangeText={text => setUsername(text)}
-        style={myStyle.pocket}
-           />
-             <TextInput
-         value={password}
-         placeholder="Password"
-         onChangeText={text => setPassword(text)}
-        style={myStyle.pocket}
-           />
-        <TextInput
-        style={myStyle.pocket}
-        value={confirm_password}
-        placeholder="confirm Password"
-        onChangeText={text => setConfirm_password(text)}
-       />
-      
-        <TouchableOpacity style={myStyle.add_pocket} onPress={register} >
-          <Text >Login</Text>
-        </TouchableOpacity>
-           </View>
 
+          <Text style={{ fontSize: 32 }}>I have money</Text>
+          <TextInput
+            value={email}
+            placeholder="email"
+            onChangeText={text => setEmail(text)}
+            style={myStyle.pocket}
+          />
+          <TextInput
+            value={username}
+            placeholder="Username"
+            onChangeText={text => setUsername(text)}
+            style={myStyle.pocket}
+          />
+          <TextInput
+            value={password}
+            placeholder="Password"
+            onChangeText={text => setPassword(text)}
+            style={myStyle.pocket}
+          />
+          <TextInput
+            style={myStyle.pocket}
+            value={confirm_password}
+            placeholder="confirm Password"
+            onChangeText={text => setConfirm_password(text)}
+          />
 
-           
-        <TouchableOpacity onPress={validateEmail}>
-          <Text >Register</Text>
+          <TouchableOpacity style={myStyle.add_pocket} onPress={register} >
+            <Text >Sign in</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity onPress={register}>
+          <Text >Cancle</Text>
         </TouchableOpacity>
-         
+
       </View>
 
+    </LinearGradient>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            <TextInput
-              value={email}
-              placeholder="email"
-              onChangeText={text => setEmail(text)}
-            />
-            <TextInput
-              value={username}
-              placeholder="Username"
-              onChangeText={text => setUsername(text)}
-            />
-            <TextInput
-              value={password}
-              placeholder="Password"
-              onChangeText={text => setPassword(text)}
-            />
-            <TextInput
-              value={confirm_password}
-              placeholder="confirm Password"
-              onChangeText={text => setConfirm_password(text)}
-            />
-             <View >
-              <TouchableOpacity >
-                <Text onPress={register} >
-                  Register</Text>
-              </TouchableOpacity>
-              <TouchableOpacity >
-                <Text onPress={() => router.back()} >Cancel</Text>
-              </TouchableOpacity>
-            </View>
-        </LinearGradient>
-
-        
-    );
-  }
+  );
+}
